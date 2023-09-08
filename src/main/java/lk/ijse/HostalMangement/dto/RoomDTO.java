cb9b87151af8531058d40b182d0e3bcac3eb7dc2
TashiniWijesekara <wijesekaratashini@gmail.com>
2023-09-08 10:08:26 +0530

    first commit

---
+++ src/main/java/lk/ijse/HostalMangement/dto/RoomDTO.java
@@ -0,0 +1,29 @@
+package lk.ijse.HostalMangement.dto;
+
+import lk.ijse.HostalMangement.entity.RoomEntity;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RoomDTO {
+
+    private String RoomTypeId;
+    private String Type;
+    private String KeyMoney;
+    private int Qty;
+    //private List<ReservationEntity> reservationEntities = new ArrayList<>();
+
+    public RoomEntity ToEntity(){
+        RoomEntity room = new RoomEntity();
+        room.setRoomTypeId(this.RoomTypeId);
+        room.setType(this.Type);
+        room.setKeyMoney(this.KeyMoney);
+        room.setQty(this.Qty);
+        //room.setReservationEntities(this.reservationEntities);
+        return room;
+    }
+
+}
